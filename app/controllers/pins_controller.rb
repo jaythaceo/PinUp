404f320d44b882d90826aa2dcde1059a818a5530
jaythaceo <jaythaceo@gmail.com>
2015-02-02 14:21:37 -0500

    [chore] Added methods to Pins controller

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,6 +1,34 @@
 class PinsController < ApplicationController
 
-	def index
-		
+	def index	
+	end
+
+	def show
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+	
+	def upvote
+	end
+
+	private
+
+	def pin_parms
+	end
+
+	def find_pin
 	end
 end
